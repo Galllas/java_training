0c62aea68d7aca88d3b7b495d57cef475d8c2001
gz475 <gz475@nyu.edu>
2015-09-04 14:09:24 -0400

    update

--- java_training/src/com/fdmgroup/cracking/SumLists/SumLists.java
+++ java_training/src/com/fdmgroup/cracking/SumLists/SumLists.java
@@ -6,34 +6,43 @@ public class SumLists {
 	
 	public static Node sumReverseLists(Node l1, Node l2){
 		
+		Node head = new Node(0);
+		Node temp = head;
+		int next = 0;
 		
+		while ( l1 != null || l2 != null || next!= 0){
+			
+			int v1 = l1 != null ? l1.data : 0;
+			int v2 = l2 != null ? l2.data : 0;
+			
+			int sum = v1 + v2;
+			int val = sum%10 + next;
+			
+			Node add = new Node(val);
+			head.next = add;
+			head = head.next;
+			
+			next = sum/10;
+			if (l1 != null) l1 = l1.next;
+			if (l2 != null) l2 = l2.next;
+	
+		}
 		
-		
-		
-		
-		
-		return l1;
+		return temp.next;
 	}
 	
-	public static Node sumForwardLists(Node l1, Node l2){
-		
-		
-		
-		
-		return l1;
-	}	
 	
 	public static void main(String[] args) {
 		
 		Node l1 = new Node(1);
+		l1.appendToTail(5);
 		l1.appendToTail(9);
-		l1.appendToTail(3);
 		Node l2 = new Node(8);
-		l2.appendToTail(7);
+		l2.appendToTail(9);
 		l2.appendToTail(5);
 
 		Node s1 = sumReverseLists(l1, l2);
-		Node s2 = sumForwardLists(l1, l2);
+	//	Node s2 = sumForwardLists(l1, l2);
 		
 		while (s1 != null){
 			
@@ -41,12 +50,12 @@ public class SumLists {
 			s1 = s1.next;
 			
 		}
-		while (s2 != null){
-			
-			System.out.println(s2.data);
-			s2 = s2.next;
-			
-		}
+//		while (s2 != null){
+//			
+//			System.out.println(s2.data);
+//			s2 = s2.next;
+//			
+//		}
 	
 	}
 
