7d72df9e0a74899eb964eba974a72a852596abc9
gz475 <gz475@nyu.edu>
2015-09-06 17:55:41 -0400

    delete cracking

--- java_training/src/com/fdmgroup/cracking/AnimalShelter/Cat.java
+++
@@ -1,9 +0,0 @@
-package com.fdmgroup.cracking.AnimalShelter;
-
-public class Cat extends Animal{
-
-	public Cat(String name) {
-		super(name);
-	}
-
-}
--- java_training/src/com/fdmgroup/cracking/FixedMultiStack/FullStackException.java
+++
@@ -1,5 +0,0 @@
-package com.fdmgroup.cracking.FixedMultiStack;
-
-public class FullStackException extends Exception {
-	
-}
